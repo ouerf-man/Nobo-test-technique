9c4d25ba6a8afaf68444ff7bab2bc4a407692830
Raed Ouerfelli <werfelli.raed@yahoo.fr>
2019-10-20 22:26:24 +0200

    1st modifications

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,16 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import { Link } from 'react-router-dom'
 const NavBar = () => {
     return(
         <div style={{}}>
         <AppBar style={{backgroundColor:"rgb(255, 168, 168)"}} position="fixed">
             <Toolbar>
                 <Typography variant="title" color="inherit">
-                    <img src="https://nobo.life/img/nobo/logo/logo-nobo-black-bold-cropped-padded-small.png" />
+                <Link to="/">
+                        <img src={"https://nobo.life/img/nobo/logo/logo-nobo-black-bold-cropped-padded-small.png"}  alt="logo" width={80}/>
+                </Link>           
                 </Typography>
             </Toolbar>
         </AppBar>
