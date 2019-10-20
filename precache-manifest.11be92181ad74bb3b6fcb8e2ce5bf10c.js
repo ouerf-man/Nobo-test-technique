ffc4d671262ca209df916b172806599eab3cd5c2
Raed Ouerfelli <werfelli.raed@yahoo.fr>
2019-10-20 22:23:51 +0200

    Updates

---
+++ precache-manifest.11be92181ad74bb3b6fcb8e2ce5bf10c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17be40d3ee727254974f3f73e511e8dd",
+    "url": "/Nobo-test-technique/index.html"
+  },
+  {
+    "revision": "d58f6650f6e235f1c23e",
+    "url": "/Nobo-test-technique/static/css/main.e5dca79a.chunk.css"
+  },
+  {
+    "revision": "d6418ac2cb7a3e93475e",
+    "url": "/Nobo-test-technique/static/js/2.72729ec5.chunk.js"
+  },
+  {
+    "revision": "d58f6650f6e235f1c23e",
+    "url": "/Nobo-test-technique/static/js/main.b8916fdd.chunk.js"
+  },
+  {
+    "revision": "636e74126e9abea68d12",
+    "url": "/Nobo-test-technique/static/js/runtime~main.83da1d9b.js"
+  }
+]);
